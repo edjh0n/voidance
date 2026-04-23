89803779c11a0f30e44fb8eccf2d0df231ce11ff
edjh0n <82029516+edjh0n@users.noreply.github.com>
2026-04-24 01:42:42 +0800

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Nav          from './components/Nav'
-import Hero         from './components/Hero'
-import About        from './components/About'
-import Members      from './components/Members'
-import Discography  from './components/Discography'
-import Tour         from './components/Tour'
-import Gallery      from './components/Gallery'
+import Nav            from './components/Nav'
+import Hero           from './components/Hero'
+import About          from './components/About'
+import Members        from './components/Members'
+import Discography    from './components/Discography'
+import Tour           from './components/Tour'
+import Gallery        from './components/Gallery'
 // import Media        from './components/Media'
-import Contact      from './components/Contact'
-import Footer       from './components/Footer'
-import MusicPlayer  from './components/MusicPlayer'
-import Starfield    from './components/Starfield'
+import Contact        from './components/Contact'
+import Footer         from './components/Footer'
+import MusicPlayer    from './components/MusicPlayer'
+import Starfield      from './components/Starfield'
+import { Analytics }  from "@vercel/analytics/next"
 
 export default function App() {
   return (
