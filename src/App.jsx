1b3821141c38bd5919a287c5974ed1fc4fe60683
Baquero <Ed.Baquero@zywave.com>
2026-04-24 15:04:26 +0800

    redeploy for audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import Nav            from './components/Nav'
-import Hero           from './components/Hero'
-import About          from './components/About'
-import Members        from './components/Members'
-import Discography    from './components/Discography'
-import Tour           from './components/Tour'
-import Gallery        from './components/Gallery'
+import Nav                from './components/Nav'
+import Hero               from './components/Hero'
+import About              from './components/About'
+import Members            from './components/Members'
+import Discography        from './components/Discography'
+import Tour               from './components/Tour'
+import Gallery            from './components/Gallery'
 // import Media        from './components/Media'
-import Contact        from './components/Contact'
-import Footer         from './components/Footer'
-import MusicPlayer    from './components/MusicPlayer'
-import Starfield      from './components/Starfield'
-import { Analytics }  from "@vercel/analytics/react"
+import Contact            from './components/Contact'
+import Footer             from './components/Footer'
+import MusicPlayer        from './components/MusicPlayer'
+import Starfield          from './components/Starfield'
+import { Analytics }      from "@vercel/analytics/react"
+import { SpeedInsights }  from "@vercel/speed-insights/react"
 
 export default function App() {
   return (
@@ -30,6 +31,7 @@ export default function App() {
       <Footer />
       <MusicPlayer />
       <Analytics />
+      <SpeedInsights/>
     </>
   )
 }
